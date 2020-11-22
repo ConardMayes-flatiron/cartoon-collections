752b00b14fa5f8fa44ed1908f5868079e975c00f
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-22 00:15:01 -0600

    commit tree 24b2b7b9db2da4516f89b7884a9e960f32783294

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |item, index|
+    puts "#{index+1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+# binding.pry
+
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  yells = []
+  array.map do |yell|
+    new_yell=[]
+    command = yell.split("")
+    new_yell << (command.shift).upcase
+    # binding.pry
+    new_yell << command
+    new_yell << "!"
+    yells << new_yell.join("")
+  end
+  yells
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  if array.find {|i| i.length > 4 } != nil
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|i| cheese_types.include?(i)}
 end
